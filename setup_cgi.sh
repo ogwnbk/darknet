72301fb575aafd6949799029dfaab33a9c47900d
ogwnbk <ubuntu@ip-172-31-33-38.ap-northeast-1.compute.internal>
2018-11-10 10:05:48 +0000

    cgiを動かすための設定を追加

--- setup_cgi.sh
+++ setup_cgi.sh
@@ -8,6 +8,11 @@ sudo chmod 777 /usr/lib/cgi-bin/
 mkfifo /usr/lib/cgi-bin/darknet_in
 mkfifo /usr/lib/cgi-bin/darknet_out
 
+cp darknet.cgi /usr/lib/cgi-bin/
+chmod 755 /usr/lib/cgi-bin/darknet.cgi
+sudo a2enmod cgid
+sudo apt install libcgi-pm-perl
+
 sudo apt install apache2
 sudo service apache2 start
 
